6671e25b5a18d5a7659e7dc61655584b4084d90d
DmitryNota <168217707+DmitryNota@users.noreply.github.com>
2024-09-25 21:08:39 +0500

    Factorial of a natural number

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,24 @@
+// Итерационный метод
+
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1) {
+    alert('Введите целое число больше 1');
+    return 1;
+  } else if (n > 1) {
+    let cycle = 1;
+    for (let i = 2; i <= n; i++) {
+      cycle = cycle * i;
+    }
+    return cycle;
+  }
+}
+
+// Рекурсивный метод
+
+function factorialReq(n) {
+  if (n === 0 || n === 1) {
+    alert('Введите целое число больше 1');
+    return 1;
+  }
+  return n * factorialReq(n - 1);
 }
