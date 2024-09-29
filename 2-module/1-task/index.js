80bccca04ef183935d94f0233bff6d63196f1291
DmitryNota <168217707+DmitryNota@users.noreply.github.com>
2024-09-29 23:30:03 +0500

    Salary calculation

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let k in salaries) {
+    if (typeof salaries[k] === 'number') {
+      if ((!isNaN(salaries[k])) && (salaries[k] !== Infinity) && (salaries[k] !== -Infinity)) {
+        sum += salaries[k];
+      }
+    }
+  }
+  return sum;
 }
