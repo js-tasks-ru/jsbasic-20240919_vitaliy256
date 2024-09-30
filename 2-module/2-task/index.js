16a0997d01a44e9a5032c88b39eb733215a82dbd
Dmitry <168217707+DmitryNota@users.noreply.github.com>
2024-09-30 14:26:45 +0500

    Is the object empty?

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,15 @@
+// Через for in
 function isEmpty(obj) {
-  // ваш код...
+  for (let prop in obj) {
+
+    return false;
+  }
+
+  return true;
+}
+
+// Через Object.keys()
+function isEmptyAnother(obj) {
+
+  return Object.keys(obj).length === 0;
 }
