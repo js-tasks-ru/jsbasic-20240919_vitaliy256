8ca3ad982492a45b42f57a209256663bd9534ef2
Dmitry <168217707+DmitryNota@users.noreply.github.com>
2024-09-30 08:34:24 +0500

    refactor: String truncate

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,10 +1,8 @@
 function sumSalary(salaries) {
   let sum = 0;
   for (let k in salaries) {
-    if (typeof salaries[k] === 'number') {
-      if ((!isNaN(salaries[k])) && (salaries[k] !== Infinity) && (salaries[k] !== -Infinity)) {
-        sum += salaries[k];
-      }
+    if (isFinite(salaries[k])) {
+      sum += salaries[k];
     }
   }
   return sum;
