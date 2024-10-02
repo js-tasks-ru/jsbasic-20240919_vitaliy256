391ce20e6fe4e954d6587f5bc5f7f85a42dc091a
Dmitry <168217707+DmitryNota@users.noreply.github.com>
2024-10-02 10:40:21 +0500

    Filtering by range

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,12 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] >= a && arr[i] <= b) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
 }
