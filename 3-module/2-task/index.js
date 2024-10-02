7e43fe28872e509bd430e995b08922a9a5b26cd4
Dmitry <168217707+DmitryNota@users.noreply.github.com>
2024-10-02 10:41:03 +0500

    Revert "Filtering by range"
    This reverts commit 391ce20e6fe4e954d6587f5bc5f7f85a42dc091a.

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,12 +1,3 @@
 function filterRange(arr, a, b) {
-  let result = [];
-
-  for (let i = 0; i < arr.length; i++) {
-
-    if (arr[i] >= a && arr[i] <= b) {
-      result.push(arr[i]);
-    }
-  }
-
-  return result;
+  // ваш код...
 }
