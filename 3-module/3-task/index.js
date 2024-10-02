75655ed7f72d9143560dca612453d5f8f81309f8
Dmitry <168217707+DmitryNota@users.noreply.github.com>
2024-10-02 11:19:12 +0500

    Kebab-case to CamelCase

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,6 @@
-function camelize(str) {
-  // ваш код...
-}
+let camelize = (str) =>
+  str.split('-')
+    .map((el, key) =>
+      key === 0
+        ? el
+        : el[0].toUpperCase() + el.slice(1)).join('');
