30c027ea5382a202eb7d4eea2e461502bf474320
DmitryNota <168217707+DmitryNota@users.noreply.github.com>
2024-10-21 23:19:21 +0500

    UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,46 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.table();
   }
+
+  thead() {
+    let thead = document.createElement('thead');
+    let tr = document.createElement('tr');
+    const params = ['Имя', 'Возраст', 'Зарплата', 'Город'];
+    tr.innerHTML = params.map((it) => `<th>${it}</th>`).join('');
+    thead.appendChild(tr);
+
+    return thead;
+  }
+
+  body() {
+    let tbody = document.createElement('tbody');
+    this.rows.map((it) => {
+      let tr = document.createElement('tr');
+      let objValues = Object.values(it);
+      tr.innerHTML = objValues.map((it) => `<td>${it}</td>`).join('');
+      let destroy = document.createElement('td');
+      destroy.innerHTML = '<button>[x]</button>';
+      tr.appendChild(destroy);
+      tbody.appendChild(tr);
+    });
+    return tbody;
+  }
+
+  table() {
+    const table = document.createElement('table');
+    table.addEventListener('click', this.click);
+    table.appendChild(this.body());
+    table.appendChild(this.thead());
+
+    return table;
+  }
+
+  click = (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      const tr = e.target.closest('tr');
+      tr.remove();
+    }}
 }
